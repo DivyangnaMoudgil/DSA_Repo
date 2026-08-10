a1af033072fc6a866f0d102a48a35dfba1c51717
Divyangna Moudgil <divyangna111105@gmail.com>
2026-08-10 19:04:33 +0530

    DSA answers 3

---
+++ lc_19.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        // Move fast pointer n+1 steps ahead
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        // Move both pointers until fast reaches end
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        // Remove nth node from end
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
